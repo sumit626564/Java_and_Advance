1f02e7614a5b3e6b5d43e3ed450b3247d2ca87e6
Sumit Singh <rajputsinghshear@gmail.com>
2025-11-06 09:22:13 +0530

    Update Merge Sort

--- JavaCore/DvideCoquer/MergeSort.java
+++ JavaCore/DvideCoquer/MergeSort.java
@@ -1,59 +1,56 @@
 package JavaCore.DvideCoquer;
 
 public class MergeSort {
-
     public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+ " ");
+        for(int i=0; i<=arr.length-1; i++){
+            System.out.print(arr[i]+" ");
         }
+        System.out.println();
     }
-    public static void mergeSort(int arr[], int starting_index, int ending_index) {
-        if(starting_index >= ending_index){    // this is our base case in recrusion
+    public static void mergeSort(int arr[], int starting_index, int end_index){
+        if(starting_index >= end_index){
             return;
         }
-        int mid_index = (starting_index + ending_index) / 2;   // step find for mid
-        mergeSort(arr, starting_index, mid_index);   // left index
-        mergeSort(arr, mid_index+1, ending_index);   // right index
-        // then we need to merge the both function
-        merge(arr, starting_index, mid_index, ending_index);    // just assume  this function is already exists. not peramenet , after decliration then we need to create a this function
+        int mid_index = (starting_index + end_index)/2;
+        mergeSort(arr, starting_index, mid_index);     // left_index 
+        mergeSort(arr, mid_index+1, end_index);         // right_index
+        Merge(arr, starting_index, mid_index, end_index);    /// Combine to all index including (arr, starting_index, mid_index, end_index)
     }
+    public static void Merge(int arr[], int starting_index, int mid_index, int end_index){
+        // create temp array
+        int[] temp = new int[end_index-starting_index+1];
+        // create index_
+        int left_index = starting_index;
+        int right_index =  mid_index+1;
+        int k_temp_index =  0;
 
-    public static void merge(int arr[], int starting_index, int mid_index, int ending_index){
-        int[] temp= new int[ending_index - starting_index+1];
-        // iterate
-        int left_part = starting_index;    // this is a iterator for left part
-        int right_part = mid_index+1;        // iterator for  right part
-        int k_temp_arr = 0;                 //  iterator for temp arr
-
-        while (left_part <= mid_index && right_part <= ending_index) {
-            // we need to comare left part element to right part of element
-            if(arr[left_part] < arr[right_part]){
-                temp[k_temp_arr] = arr[left_part];
-                left_part++;
+        while (left_index < mid_index && right_index < end_index) {
+            if(arr[left_index] <= arr[right_index]){
+                temp[k_temp_index] = arr[left_index];
+                left_index++;
             }else{
-                temp[k_temp_arr] = arr[right_part];
-                right_part++; 
+                temp[k_temp_index] = arr[right_index];
+                right_index++;
             }
-            k_temp_arr++;
+            k_temp_index++;
         }
 
-        // left part remain
-        while (left_part <= mid_index) {
-            temp[k_temp_arr++] = arr[left_part++];
+        // reamin part left part
+        while (left_index <= mid_index) {
+            temp[k_temp_index++] = arr[left_index++];
         }
-        // right part remain
-        while (right_part <= ending_index) {
-            temp[k_temp_arr++] = arr[right_part++];
+        // remain part of right part
+        while(right_index <= end_index){
+            temp[k_temp_index++] = arr[right_index++];
         }
 
-        // copy temp to orginal array
-        for(k_temp_arr=0, left_part=starting_index; k_temp_arr<temp.length; k_temp_arr++, left_part++){
-            arr[left_part] = temp[k_temp_arr];
+        // copy of temp to orginal array....!
+        for(int i=0; i<temp.length; i++){
+            arr[starting_index + i] = temp[i];
         }
     }
-
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6};
+        int[] arr = {2, 3, 4, 5, 6, 7, 8};
         mergeSort(arr, 0, arr.length-1);
         printArr(arr);
     }
